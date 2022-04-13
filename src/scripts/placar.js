5aec72e6f48165619a9bb64d216e69878f2475a8
GaabrielFranca <gabrielfran1864@gmail.com>
2022-04-13 10:40:49 -0300

    teste

--- src/scripts/placar.js
+++ src/scripts/placar.js
@@ -2,4 +2,4 @@
 // adicionar esse simbolo na div placar (criar novo estilo)
 // colocar limite de pontos. Apos o limite (md5) ser atingido,
 // aparecer um alert com  o simbolo ganhador e resetar o placar
-// 
+// teste teste tes
